b8282e96b8dba9c04a62c861f827e373dd34de05
DennsHartstock <dennis.hartstock@web.de>
2023-01-02 09:26:02 +0100

    exercise "Variables and DataTypes"

---
+++ app/src/main/java/com/commcode/basics/ExerciseValiablesAndDataTypes.kt
@@ -0,0 +1,36 @@
+package com.commcode.basics
+
+//"Android Masterclass"
+const val string: String = "Android Masterclass"
+
+//13.37F
+const val float: Float = 13.37F
+
+//3.14159265358979
+const val pi = 3.14159265358979
+
+//25
+const val byte: Byte = 25
+
+//2020
+const val short: Short = 2020
+
+//18881234567
+const val long: Long = 18881234567
+
+//true
+const val boolean: Boolean = true
+
+//'a'
+const val char: Char = 'a'
+
+fun main() {
+    println("String $string")
+    println("Float $float")
+    println("PI $pi")
+    println("Byte $byte")
+    println("Short $short")
+    println("Int $long")
+    println("Boolean $boolean")
+    println("Char $char")
+}
