305ea6b0d3d3e66d9345025b31cc4e5f25bcab35
DennsHartstock <dennis.hartstock@web.de>
2023-01-07 10:16:23 +0100

    using constructors and functions

--- app/src/main/java/com/commcode/basics/Person.kt
+++ app/src/main/java/com/commcode/basics/Person.kt
@@ -2,11 +2,40 @@ package com.commcode.basics
 
 fun main() {
     val me = Person("Dennis", "Hartstock")
+    me.stateHobby()
+
+    val vita = Person("Vita", "Hartstock", 13)
+    vita.stateAge()
+
     val john = Person()
-    val johnWick = Person(lastName = "Wick")
+    john.stateAge()
+
+    val johnWick = Person(_lastName = "Wick")
+    johnWick.hobby = "to kill people"
+    johnWick.stateHobby()
 }
 
-class Person(firstName: String = "John", lastName: String = "Smith") {
+class Person(_firstName: String = "John", _lastName: String = "Smith") {
+
+    // Member variables -> Properties
+    private var firstName = _firstName
+    private var lastName = _lastName
+    private var age: Int? = null
+    var hobby: String = "Powerlifting"
+
+    // Member secondary constructor
+    constructor(_firstName: String, _lastName: String, _age: Int) : this(_firstName, _lastName) {
+        this.age = _age
+    }
+
+    // Member functions -> Methods
+    fun stateHobby() {
+        println("The hobby of $firstName $lastName is $hobby")
+    }
+
+    fun stateAge() {
+        println("$firstName $lastName is $age years old")
+    }
 
     init {
         println("Initialized new Person object with first name: $firstName and last name: $lastName")
