668c927b73ce3f88a221577f3c7c5b38a452fad2
DennsHartstock <dennis.hartstock@web.de>
2023-01-11 08:06:08 +0100

    update Exercise "ArrayLists"

--- app/src/main/java/com/commcode/basics/ExerciseArrayLists.kt
+++ app/src/main/java/com/commcode/basics/ExerciseArrayLists.kt
@@ -7,5 +7,15 @@ fun main() {
     numbers.add(2166.979)
     numbers.add(15798.654)
     numbers.add(1543.389)
+
+    var sum = 0.0
+    for (number in numbers) {
+        sum += number
+    }
+    val average = sum / numbers.size
+    println("Average of numbers in ArrayList is $average")
+
     println("Average of numbers in ArrayList is ${numbers.sum() / numbers.size}")
+
+    println("Average of numbers in ArrayList is ${numbers.average()}")
 }
